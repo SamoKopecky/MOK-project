c044523accadcc07247b80efec3ce02760a42764
SamoKopecky <skopecky@redhat.com>
2022-11-08 22:03:16 +0100

    feat: Fix some things, still not working
    Signed-off-by: SamoKopecky <skopecky@redhat.com>

--- common.py
+++ common.py
@@ -1,4 +1,4 @@
-from hashlib import sha256, shake_128
+from hashlib import shake_256
 from typing import Callable, Any, List
 
 import numpy as np
@@ -11,7 +11,7 @@ from params import *
 def generate_ring_vec():
     ring = []
     for i in range(M - 1):
-        ring.append(Poly(np.random.randint(low=-Q / 2, high=Q / 2, size=N)))
+        ring.append(Poly(np.random.randint(low=-12418, high=12418, size=N)))
     return ring
 
 
@@ -21,22 +21,13 @@ def ring_mul_mod(a, b, poly_mod, mod):
     return Poly(reminder)
 
 
-def ring_vec_scalar_vec(ring_vec, scalar):
-    ring_vec_copy = ring_vec.copy()
-    for i in range(len(ring_vec)):
-        ring_vec_copy[i] *= scalar[i]
-        ring_vec_copy[i].coef %= 2 * Q
-    return ring_vec_copy
-
-
 def ring_vec_ring_mul_mod(ring_vec, ring, poly_mod, mod):
-    result_ring = np.zeros(N)
+    result_ring = []
     for i in range(len(ring_vec)):
         poly_factor = ring_mul_mod(ring_vec[i], ring, poly_mod, mod)
-        for j in range(N):
-            result_ring[j] += poly_factor.coef[j]
-            result_ring[j] %= mod
-    return Poly(result_ring)
+        poly_factor.coef %= mod
+        result_ring.append(poly_factor)
+    return result_ring
 
 
 def ring_vec_ring_vec_mul_mod(a, b, poly_mod, mod):
@@ -76,32 +67,34 @@ def lift(ring_vec: List[Poly], ring: Poly):
     ring_copy = ring.copy()
     ring_copy = scalar_ring_mul(ring_copy, -2)
     ring_copy = scalar_ring_add(ring_copy, Q)
+    ring_copy.coef %= 2 * Q
     return transform_ring_vec(ring_vec, scalar_ring_mul, 2) + [ring_copy]
 
 
 def h_one(
         big_l: List[Poly],
         big_h_2q: List[Poly],
-        message: np.ndarray,
+        message: int,
         first: Poly,
         second: Poly,
 ):
-    sha = sha256()
+    shake = shake_256()
     for pub_key in big_l:
-        sha.update(pub_key.coef)
-    sha.update(message)
+        shake.update(pub_key.coef)
+    shake.update(bytes(message))
     for i in range(M):
-        sha.update(big_h_2q[i].coef)
-    sha.update(first.coef)
-    sha.update(second.coef)
-    digest = sha.digest()
-    shake = shake_128()
-    shake.update(digest)
+        shake.update(big_h_2q[i].coef)
+    shake.update(first.coef)
+    shake.update(second.coef)
     return shake.digest(int(N * 14 / 8))
 
 
 def random_byte_vectors():
-    return Poly(np.random.randint(low=0, high=255, size=N))
+    vectors = []
+    for i in range(M):
+        vectors.append(Poly(np.random.normal(0, SIGMA, size=N)))
+        # vectors.append(Poly([1 for _ in range(N)]))
+    return vectors
 
 
 def convert_bytes_to_poly(input_bytes):
@@ -110,8 +103,8 @@ def convert_bytes_to_poly(input_bytes):
     for i in range(len(input_bytes) * 8):
         bits.append((input_bytes[int(i / 8)] // 2 ** (i % 8)) % 2)
     for i in range(N):
-        bitstring = ''
-        for j in range(i*14, 14*(i + 1)):
+        bitstring = ""
+        for j in range(i * 14, 14 * (i + 1)):
             bitstring += str(bits[j])
-        ring.append(int(bitstring, 2) % Q)
+        ring.append(int(bitstring, 2) % (2 * Q))
     return Poly(ring)
--- main.py
+++ main.py
@@ -5,11 +5,9 @@ from KeyPair import KeyPair
 from PubParams import PubParams
 from common import (
     ring_vec_ring_vec_mul_mod,
-    ring_vec_scalar_vec,
     lift,
     h_one,
     random_byte_vectors,
-    scalar_ring_mul,
     convert_bytes_to_poly,
     ring_vec_ring_mul_mod,
 )
@@ -22,50 +20,55 @@ def key_gen(poly_mod, pub_params):
 
 def sign(poly_mod, pi, message, key_pairs, pub_params):
     big_s_pi = key_pairs[pi].private_key
-    big_s_pi_2q = lift(big_s_pi, Poly([1 for _ in range(N)]))
+    big_s_pi_2q = big_s_pi + [Poly([1 for _ in range(N)])]
     big_l = [pair.public_key for pair in key_pairs]
 
     h = ring_vec_ring_vec_mul_mod(pub_params.big_h, big_s_pi, poly_mod, Q)
 
     big_h_2q = lift(pub_params.big_h, h)
     big_a_pi_2q = lift(pub_params.big_a, key_pairs[pi].public_key)
+
     u = random_byte_vectors()
 
     t = [0 for _ in range(W)]
     c = [0 for _ in range(W)]
-    c[pi + 1] = h_one(
+    c[(pi + 1) % W] = h_one(
         big_l,
         big_h_2q,
         message,
-        ring_vec_ring_mul_mod(big_a_pi_2q, u, poly_mod, Q),
-        ring_vec_ring_mul_mod(big_h_2q, u, poly_mod, Q),
+        ring_vec_ring_vec_mul_mod(big_a_pi_2q, u, poly_mod, Q),
+        ring_vec_ring_vec_mul_mod(big_h_2q, u, poly_mod, Q),
     )
 
     for i in range(pi + 1, pi + W):
         i %= W
         i_plus_one = (i + 1) % W
+        print(i, i_plus_one)
         big_a_i_2q = lift(pub_params.big_a, big_l[i])
         t[i] = random_byte_vectors()
         q_times_ci = convert_bytes_to_poly(c[i]) * Q
-        first = q_times_ci + ring_vec_ring_mul_mod(big_a_i_2q, t[i], poly_mod, Q)
-        second = q_times_ci + ring_vec_ring_mul_mod(big_h_2q, t[i], poly_mod, Q)
+        first = q_times_ci + ring_vec_ring_vec_mul_mod(big_a_i_2q, t[i], poly_mod, Q)
+        second = q_times_ci + ring_vec_ring_vec_mul_mod(big_h_2q, t[i], poly_mod, Q)
         c[i_plus_one] = h_one(big_l, big_h_2q, message, first, second)
 
-    # TODO: fix this, always get -1
-    b = -(1 ** (np.random.randint(low=0, high=2, size=1)[0]))
+    b = (-1) ** (np.random.randint(low=0, high=2))
     c_pi = convert_bytes_to_poly(c[pi]) * b
-    t[pi] = ring_vec_ring_mul_mod(big_s_pi_2q, c_pi, poly_mod, Q) + u
-    return [convert_bytes_to_poly(c[1])] + t + [h]
+    result = ring_vec_ring_mul_mod(big_s_pi_2q, c_pi, poly_mod, Q)
+    for i in range(len(result)):
+        result[i] += u[i]
+    t[pi] = result
+    return [convert_bytes_to_poly(c[0])] + t + [h]
 
 
 def verify(poly_mod, signature, message, key_pairs, pub_params) -> bool:
     signed_c1 = signature[0]
-    t = signature[1: len(signature) - 1]
+    print(signed_c1.coef[:4])
+    t = signature[1 : len(signature) - 1]
     h = signature[len(signature) - 1]
     big_l = [pair.public_key for pair in key_pairs]
 
     big_h_2q = lift(pub_params.big_h, h)
-    c = [[Poly([0])] for _ in range(W)]
+    c = [0 for _ in range(W)]
     c[0] = signed_c1
 
     for i in range(W):
@@ -77,11 +80,15 @@ def verify(poly_mod, signature, message, key_pairs, pub_params) -> bool:
             q_times_ci = c[i] * Q
         else:
             q_times_ci = convert_bytes_to_poly(c[i]) * Q
-        first = q_times_ci + ring_vec_ring_mul_mod(big_a_i_2q, t[i], poly_mod, Q)
-        second = q_times_ci + ring_vec_ring_mul_mod(big_h_2q, t[i], poly_mod, Q)
+        first = q_times_ci + ring_vec_ring_vec_mul_mod(big_a_i_2q, t[i], poly_mod, Q)
+        second = q_times_ci + ring_vec_ring_vec_mul_mod(big_h_2q, t[i], poly_mod, Q)
         c[i_plus_one] = h_one(big_l, big_h_2q, message, first, second)
 
     verified_c1 = convert_bytes_to_poly(c[0])
+    for a in c:
+        print(convert_bytes_to_poly(a).coef[:4])
+    print(verified_c1.coef[:4])
+    print(signed_c1.coef[:4])
     return verified_c1 == signed_c1
 
 
@@ -90,16 +97,18 @@ def main():
     poly_mod = Poly([0 for _ in range(N + 1)])
     poly_mod.coef[0] = 1
     poly_mod.coef[N] = 1
-    message = np.random.randint(low=0, high=255, size=255)
+    message = 10
     pi = 1  # Actually pi = 2
 
     key_pairs = key_gen(poly_mod, pub_params)
     signature = sign(poly_mod, pi, message, key_pairs, pub_params)
     verified = verify(poly_mod, signature, message, key_pairs, pub_params)
-    print(verified)
-
-    test = key_pairs[pi].public_key.coef.transpose()
-    print()
+    if verified:
+        print("###########")
+        print("###########")
+        print("###########")
+        print("###########")
+        print("###########")
 
 
 if __name__ == "__main__":
